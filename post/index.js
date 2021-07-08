747ba1541bf277426c41754fb87d0dd0f51831c5
Louis Lassegue <louislassegue@gmail.com>
2021-07-08 15:30:21 -0700

    change post file

--- post/index.js
+++ post/index.js
@@ -2,20 +2,26 @@
 
 const uuid = require('uuid').v4;
 
-const dynamoose = require('dynamoose');
+const dynamoose = require ('dynamoose');
 
-const peopleModel = require('../model/people.js');
+const peopleSchema = new dynamoose.Schema({
+  'id': String,
+  'name': String,
+  'location': String,
+  'phone': String
+});
 
+const peopleModel = dynamoose.model('PeopleTable', peopleSchema);
 
 exports.handler = async (event) => {
-
+console.log('Testing',event);
   try {
     const { name, location, phone } = JSON.parse(event.body);
 
     const id = uuid();
 
     const record = new peopleModel({ id, name, location, phone });
-    const data = await record.create();
+    const data = await record.save();
 
     return {
       statusCode: 200,
